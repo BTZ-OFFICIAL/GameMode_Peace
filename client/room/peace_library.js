337ca6afe5603c8972e9c79af184ed695ba756cf
BTZ-OFFICIAL <baktybektagaev50@gmail.com>
2024-11-27 17:47:25 +0300

    Update peace_library.js

--- client/room/peace_library.js
+++ client/room/peace_library.js
@@ -5,7 +5,9 @@ import * as teams from './default_teams.js';
 function set_inventory() {
     const context = room.Inventory.GetContext();
     context.Main.Value = true;
+    context.MainInfinity.Value = true;
     context.Secondary.Value = true;
+    context.SecondaryInfinity.Value = true;
     context.Melee.Value = true;
     context.Explosive.Value = false;
     context.Build.Value = true;
